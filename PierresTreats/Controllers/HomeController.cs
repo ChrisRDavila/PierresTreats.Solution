fafb56aae29190ee8e6914c6d3adcb7e943d74f4
ChrisRDavila <crossdavila@gmail.com>
2023-06-02 10:05:40 -0700

    set up home controller

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,13 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
-
-
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PierresTreats.Controllers
 {
-  public class HomeController : Controller
-  {
-    [Route("/")]
-    // Notice the changes below!
-    public ActionResult File() { return View(); }
-  }
+    public class HomeController : Controller
+    {
+      private readonly PierresTreatsContext _db;
+      private readonly UserManager<ApplicationUser> _userManager;
+
+      public HomeController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+      {
+        _userManager = userManager;
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public async Task<ActionResult> Index()
+      {
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        if (currentUser != null)
+        {
+          Treats[] treats = _db.Treats
+                      .Where(entry => entry.User.Id == currentUser.Id)
+                      .ToArray();
+          model.Add("treats", treats);
+          Flavors[] flavors = _db.Flavors
+                      .Where(entry => entry.User.Id == currentUser.Id)
+                      .ToArray();
+          model.Add("flavors", flavors);
+        }
+        return View(model);
+      }
+    }
 }
